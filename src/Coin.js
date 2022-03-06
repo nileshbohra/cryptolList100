9d049b10682a94bd8718e31e92a9dea98505fc0b
nileshbohra <nmbohra2001@gmail.com>
2022-03-06 13:00:55 +0530

    initial commit

--- src/Coin.js
+++ src/Coin.js
@@ -1,30 +1,31 @@
 import React from 'react'
 import './Coin.css'
 
-const Coin = ({image,name,price,volume,pricechange,marketcap}) => {
+const Coin = ({ image, name, price, rank, volume, pricechange, marketcap }) => {
+    let inr = Intl.NumberFormat('en-IN');
     return (
         <div className="coin-container">
             <div className="coin-row">
+                <div className='coin-pos'>{rank}</div>
                 <div className="coin">
                     <img src={image} alt="crypto" />
                     <h1>{name}</h1>
                     <p className="coin-symbol"></p>
                 </div>
                 <div className="coin-data">
-                    <p className="coin-price">Rs.{price}</p>
-//                     <p className="coin-volume">Rs.{volume.toLocaleString()}</p>
-                    {pricechange<0 ? (
+                    <p className="coin-price">₹{inr.format(price)}</p>
+                    {pricechange < 0 ? (
                         <p className="coin-percent red">{pricechange.toFixed(2)}%</p>
-                    ):(
+                    ) : (
                         <p className="coin-percent green">{pricechange.toFixed(2)}%</p>
                     )
-                }
-                <p className="coin-marketcap">
-                    Mkt Cap: Rs.{marketcap.toLocaleString()}
-                </p>
+                    }
+                    <p className="coin-marketcap">
+                        ₹ {inr.format(marketcap)}
+                    </p>
                 </div>
             </div>
-            
+
         </div>
     )
 }
